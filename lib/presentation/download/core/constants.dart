0695da107c7b881114974a86f12af3c13f802ef6
salahvk <salahkoyilandy@gmail.com>
2022-04-01 05:57:19 +0530

    Home screen added to netflix app

--- lib/presentation/download/core/constants.dart
+++ lib/presentation/download/core/constants.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 const kwidth = SizedBox(width: 15);
+
+const kwidth2 = SizedBox(width: 35);
 const kheight = SizedBox(height: 15);
 const kheight2 = SizedBox(height: 35);
+
+// Images
+
+const String image1 =
+    "https://th.bing.com/th/id/OIP.kZ5pi91lDwwFZolIwl5gmwHaJQ?pid=ImgDet&rs=1";
+const String image2 =
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/izIMqapegdEZj0YVDyFATPR8adh.jpg";
+const String netflix =
+    "https://cdn.vox-cdn.com/thumbor/rtSiql54y52-Lh1Y2yJt2oLfcdc=/39x0:3111x2048/1400x933/filters:focal(39x0:3111x2048):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png";
--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,14 +1,249 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:netflix/core_items/color.dart';
+import 'package:netflix/presentation/download/core/color.dart';
+import 'package:netflix/presentation/download/core/constants.dart';
+import 'package:netflix/presentation/home/numberBanner.dart';
+
+ValueNotifier<int> scrollNotifier = ValueNotifier<int>(2);
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: Text('Home'),
+      body: SafeArea(
+        child: ValueListenableBuilder<int>(
+          valueListenable: scrollNotifier,
+          builder: (context, index, _) {
+            return NotificationListener<UserScrollNotification>(
+              onNotification: (notification) {
+                final ScrollDirection directions = notification.direction;
+                print(directions);
+                if (directions == ScrollDirection.reverse) {
+                  scrollNotifier.value = 1;
+                  print('object');
+                } else if (directions == ScrollDirection.forward) {
+                  scrollNotifier.value = 2;
+                  print('no object');
+                }
+                return true;
+              },
+              child: Stack(
+                children: [
+                  ListView(
+                    children: [
+                      MainBanner(),
+                      Header(
+                        head: 'Released in the Past Year',
+                      ),
+                      ScrollBanners(width: width),
+                      Header(head: "Trending Now"),
+                      ScrollBanners(width: width),
+                      Header(head: 'Top 10 Tv Shows in India Today'),
+                      NumberBanners(width: width),
+                      kheight,
+                      ScrollBanners(width: width)
+                    ],
+                  ),
+                  if (scrollNotifier.value == 2)
+                    TopContainer()
+                  else if (scrollNotifier.value == 1)
+                    kheight
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class TopContainer extends StatelessWidget {
+  const TopContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(seconds: 5),
+      width: double.infinity,
+      height: 105,
+      color: Colors.black.withOpacity(0.2),
+      child: Column(
+        children: [
+          kheight,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    netflix,
+                  ),
+                ),
+                const Spacer(),
+                const Icon(
+                  Icons.cast,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                kwidth,
+                Container(
+                  color: Colors.blue,
+                  width: 30,
+                  height: 30,
+                )
+              ],
+            ),
+          ),
+          kheight,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'TV Shows',
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              Text('Movies', style: TextStyle(fontWeight: FontWeight.w600)),
+              Text('Categories', style: TextStyle(fontWeight: FontWeight.w600))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MainBanner extends StatelessWidget {
+  const MainBanner({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 600,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(image2), fit: BoxFit.cover)),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              HomeButtons(
+                icon: Icons.add,
+                text: 'My List',
+              ),
+              TextButton.icon(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(kwhite)),
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.play_arrow,
+                    color: kblack,
+                  ),
+                  label: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 13),
+                    child: Text(
+                      'Play',
+                      style: TextStyle(color: kblack),
+                    ),
+                  )),
+              HomeButtons(icon: Icons.info, text: 'Info')
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class HomeButtons extends StatelessWidget {
+  const HomeButtons({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: kwhite,
+        ),
+        Text(
+          text,
+          style: TextStyle(fontWeight: FontWeight.w800),
+        )
+      ],
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({
+    Key? key,
+    required this.head,
+  }) : super(key: key);
+  final String head;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Text(
+            head,
+            style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ScrollBanners extends StatelessWidget {
+  const ScrollBanners({
+    Key? key,
+    required this.width,
+  }) : super(key: key);
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return LimitedBox(
+      maxHeight: 200,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: List.generate(
+          10,
+          (index) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: width * 0.3,
+              height: width * 0.5,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover, image: NetworkImage(image1))),
+            ),
+          ),
+        ),
       ),
     );
   }
